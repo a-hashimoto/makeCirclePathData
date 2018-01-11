6b70b6945dd71258454c60e9e9676814174cd4aa
a-hashimoto <a-hashimoto@unext.jp>
2018-01-11 14:58:13 +0900

    パスの開始地点を上からできるものを追加

--- app/src/main/java/com/example/a_hashimoto/makecirclepathdata/MainActivity.java
+++ app/src/main/java/com/example/a_hashimoto/makecirclepathdata/MainActivity.java
@@ -13,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
     double x;
     double y;
     double r;
+    double rr;
+    double controlR;
+    double controlRr;
+    double zero = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,20 +32,32 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 r = Double.parseDouble(String.valueOf(radius.getText()));
+                rr = 2 * r;
+                controlR = 0.45 * r;
+                controlRr = rr - controlR;
                 x = Double.parseDouble(String.valueOf(coordinateX.getText()));
                 y = Double.parseDouble(String.valueOf(coordinateY.getText()));
-                pathData.setText(calculatePathData());
-                Log.d("calculate", calculatePathData());
+                pathData.setText(calculatePathDataFromTop());
+                Log.d("calculate", calculatePathDataFromTop());
             }
         });
     }
 
-    private String calculatePathData() {
-        String command1 = createCommandM(0, r);
+    private String calculatePathDataFromSide() { // 開始地点左
+        String command1 = createCommandM(zero, r);
         String command2 = createCommandC(0, 0.45 * r, 0.45 * r, 0, r, 0);
         String command3 = createCommandC(1.55 * r, 0, 2 * r, 0.45 * r, 2 * r, r);
         String command4 = createCommandC(2 * r, 1.55 * r, 1.55 * r, 2 * r, r, 2 * r);
         String command5 = createCommandC(0.45 * r, 2 * r, 0, 1.55 * r, 0, r);
+        return command1 + command2 + command3 + command4 + command5;
+    }
+
+    private String calculatePathDataFromTop() { // 開始地点上
+        String command1 = createCommandM(r, zero);
+        String command2 = createCommandC(controlRr, zero, rr, controlR, rr, r);
+        String command3 = createCommandC(rr, controlRr, controlRr, rr, r, rr);
+        String command4 = createCommandC(controlR, rr, zero, controlRr, zero, r);
+        String command5 = createCommandC(zero, controlR, controlR, zero, r, zero);
         return command1 + command2 + command3 + command4 + command5;
     }
 
